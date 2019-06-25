0e41bbe888365f5631f02108d54fb3dee711c828
Andrei Gramakov <mail@agramakov.me>
2019-06-25 08:57:28 +0300

    Added Json class, removed Templaer class

--- pag/__init__.py
+++ pag/__init__.py
@@ -4,8 +4,9 @@ from . import conv
 from . import image_tools
 from .class_path import Path, PathType
 from .class_args import Args
+from .class_json import Json
 from .class_serial_interface import SerialInterface
 from .class_dsvfile import DsvFile
 from .class_timermulth import TimerMulTh
 from .class_docx import Docx
-from .class_templater import Templater
+from .class_json import Json
